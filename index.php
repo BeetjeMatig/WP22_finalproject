5e815af84d2b9ff44ccd147b36edb86ca03d31c7
BeetjeMatig <justinmulder0808@gmail.com>
2022-06-11 11:55:25 +0200

    Added blockquote element and textarea

--- index.php
+++ index.php
@@ -11,6 +11,16 @@ include __DIR__ . '/tpl/head.php';
 include __DIR__ . '/tpl/body_start.php';
 ?>
 
+<div>
+    <blockquote class="blockquote">
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ad fugiat vel deleniti odio libero, porro eaque. Architecto, ratione eos impedit molestias dolorum pariatur! Voluptatem alias minus corporis illum et.</p>
+    </blockquote>
+</div>
+<div>
+    <textarea class="form-control" id="inputArea" rows="3"></textarea>
+</div>
+
+
 <?php
 include __DIR__ . '/tpl/body_end.php';
 ?>

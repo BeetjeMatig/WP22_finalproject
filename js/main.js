c6c907e29b341b6321567960b1abfbeeb5752e82
BeetjeMatig <justinmulder0808@gmail.com>
2022-06-23 15:38:18 +0200

    some js code to calculate scores

--- js/main.js
+++ js/main.js
@@ -27,9 +27,46 @@ function changespan (keycode) {
         $('#highlight').append(newText);
         console.log(character)
     }
+    if (checkWinCondition() == 1) {
+        createScore(new Date().getTime());
+    }
+}
+
+function timer (index) {
+    if (index == 0) {
+        var start = new Date().getTime();
+        return start
+    } else if (index == 1) {
+        var time = new Date().getTime();
+        return time
+    }
+}
+
+function checkWinCondition () {
+    if ($('#original').text().length == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function createScore (time) {
+    var obj = {
+        id: 1,
+        score: 2
+    }
+    const fs = require("fs");
+    let scoresJson = fs.readFileSync("../json/scores.json","utf-8");
+    let scores = JSON.parse(scoresJson);
+    users.push(obj);
+    scoresJson = JSON.stringify(scores);
 }
 
 $(document).keyup(function (event) {
+    if ($('#highlight').text().length == 1) {
+        var start = timer(0)
+    }
     var keycode = event.key;
     validateInput(keycode);
+    timer();
 });

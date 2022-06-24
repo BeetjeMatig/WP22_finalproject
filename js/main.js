9a61dcad4191857ea8773e46b9da6feba36ff44e
dienke <dienketimmer@hotmail.nl>
2022-06-24 13:08:02 +0200

    edited main.js

--- js/main.js
+++ js/main.js
@@ -40,38 +40,75 @@ function changespan (keycode) {
         $('#highlight').append(newText);
     }
     if (checkWinCondition() == 1) {
-        createScore(new Date().getTime());
+        // createScore(new Date().getTime());
+        createScore(index);
     }
 }
 
-function timer (index) {
-    if (index == 0) {
-        var start = new Date().getTime();
-        return start
-    } else if (index == 1) {
-        var time = new Date().getTime();
-        return time
-    }
-}
+// function timer (index) {
+//     if (index == 0) {
+//         var start = new Date().getTime();
+//         return start
+//     } else if (index == 1) {
+//         var time = new Date().getTime();
+//         return time
+//     }
+// }
 
 function checkWinCondition () {
-    if ($('#original').text().length == 0) {
+    if ($('#original').text().length === 0) {
         return 1;
     } else {
         return 0;
     }
 }
 
-function createScore (time) {
+// array met index id, progress, --> jquery, original and higlight, start time, stop time
+// function createScore (index) {
+//     let progress = $('#progress').text();
+//     let original = $('#original').text();
+//     let highlight = $('#highlight').text();
+//     let start = $('#start').text();
+//     let stop = $('#stop').text();
+//     let score = {
+//         id: index,
+//         progress: progress,
+//         original: original,
+//         highlight: highlight,
+//         start: start,
+//         stop: stop
+//     }
+//     scores.push(score);
+//     console.log(scores);
+// }
+function createScore (index){ //, time) {
+    let original = $('#original').text().length;
+    let highlight = $('#highlight').text().length;
+    let score = highlight / (original + highlight) * 100;
+    const fs = require('fs');
     var obj = {
         id: 1,
-        score: 2
+        score: score,
+        startTime: null,
+        stopTime: null
     }
+    if (index === 0) {
+        obj.startTime = new Date().getTime();
+    } else if (index === 1) {
+        obj.stopTime = new Date().getTime();
+    }
+    scores.push(obj);
+    // const fs = require("fs");
+    // let scoresJson = fs.readFileSync("../json/scores.json","utf-8");
+    // let scores = JSON.parse(scoresJson);
+    // users.push(obj);
+    // scoresJson = JSON.stringify(scores);
 }
 
 $(document).keyup(function (event) {
     if ($('#highlight').text().length == 1) {
-        var start = timer(0)
+        // var start = timer(0)
+        createScore(0);
     }
     var keycode = event.key;
     validateInput(keycode);

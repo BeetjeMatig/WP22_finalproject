7695789f75ab92149b1950db8de00081ae2355dd
dienke <dienketimmer@hotmail.nl>
2022-07-11 13:46:05 +0200

    update delete_data.php

--- scripts/delete_data.php
+++ scripts/delete_data.php
@@ -10,19 +10,5 @@
     fwrite($json_file_players, json_encode($playerdata));
     fclose($json_file_players);
 
-    $json_file_scores = file_get_contents("../json/scores.json");
-    $scoresdata = json_decode($json_file_scores, true);
-
-    foreach ($scoresdata as $key => $value){
-        if ($value['id'] == $_POST['score_id']){
-            unset($scoresdata[$key]);
-            break;
-        }
-    }
-
-    $json_file_scores = fopen('../json/scores.json', 'w');
-    fwrite($json_file_scores, json_encode($scoresdata));
-    fclose($json_file_scores);
-
     header("Location: ../index.php");
 ?>
